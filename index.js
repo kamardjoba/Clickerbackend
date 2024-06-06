cb66920c62f8ecaa03593d83fd35bf33c2691cb8
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-06-06 19:31:07 +0200

    initial

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const User = require('./models/user'); // Импорт модели пользо
 const app = express();
 const port = process.env.PORT || 3001; // Убедитесь, что порт установлен правильно
 const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+let bot;
+
+try {
+  bot = new TelegramBot(token, { polling: true });
+} catch (error) {
+  console.error('Telegram Bot is already running:', error.message);
+}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,27 +29,29 @@ mongoose.connect(process.env.MONGODB_URL, {
 .catch((error) => console.log(error));
 
 // Настройка Telegram Bot
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-    const username = msg.from.username;
+if (bot) {
+  bot.on('message', async (msg) => {
+      const chatId = msg.chat.id;
+      const text = msg.text;
+      const username = msg.from.username;
 
-    if (text === '/start') {
-        let user = await User.findOneAndUpdate(
-            { telegramId: chatId.toString() },
-            { telegramId: chatId.toString(), username: username, coins: 0 }, // Добавляем поле coins
-            { upsert: true, new: true }
-        );
+      if (text === '/start') {
+          let user = await User.findOneAndUpdate(
+              { telegramId: chatId.toString() },
+              { telegramId: chatId.toString(), username: username, coins: 0 }, // Добавляем поле coins
+              { upsert: true, new: true }
+          );
 
-        await bot.sendMessage(chatId, 'Добро пожаловать! Нажмите на кнопку, чтобы начать игру.', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: 'Играть', web_app: { url: `${process.env.FRONTEND_URL}?userId=${user._id}` } }]
-                ]
-            }
-        });
-    }
-});
+          await bot.sendMessage(chatId, 'Добро пожаловать! Нажмите на кнопку, чтобы начать игру.', {
+              reply_markup: {
+                  inline_keyboard: [
+                      [{ text: 'Играть', web_app: { url: `${process.env.FRONTEND_URL}?userId=${user._id}` } }]
+                  ]
+              }
+          });
+      }
+  });
+}
 
 app.get('/username', async (req, res) => {
     const userId = req.query.userId;
@@ -60,21 +68,20 @@ app.get('/username', async (req, res) => {
 });
 
 app.post('/update-coins', async (req, res) => {
-    const { id, coins } = req.body;
-    console.log(`Updating coins for user: ${id} with coins: ${coins}`); // Логируем запрос
+    const { userId, coins } = req.body;
+    console.log(`Updating coins for user: ${userId} with coins: ${coins}`); // Логируем запрос
     try {
-      const user = await User.findByIdAndUpdate(id, { coins: coins }, { new: true });
-      if (user) {
-        res.json({ success: true });
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
+        const user = await User.findByIdAndUpdate(userId, { coins: coins }, { new: true });
+        if (user) {
+            res.json({ success: true });
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
     } catch (error) {
-      console.error('Error updating coins:', error); // Логируем ошибки
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error updating coins:', error); // Логируем ошибки
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  });
-  
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
