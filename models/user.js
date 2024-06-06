3bb16848c8491c829fc6747e366e7da0291e9bf1
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-06-06 18:36:09 +0200

    initial

--- models/user.js
+++ models/user.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  telegramId: { 
-    type: String, 
-    required: true, 
-    unique: true 
-  },
-
-  username: { 
-    type: String,
-     required: true 
+    telegramId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    coins: {
+        type: Number,
+        default: 0
     }
 });
 
+module.exports = mongoose.model('User', userSchema);
 
-const User = mongoose.model('clickerbotcollections', userSchema);
+//const User = mongoose.model('clickerbotcollections', userSchema);
 
-module.exports = User;
+//module.exports = User;
