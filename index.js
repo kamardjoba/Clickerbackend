f8b70e72d726e0ae58d6df48e2f9b025877cebe0
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-06-09 22:33:23 +0200

    initial

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const User = require('./models/user');
 const axios = require('axios');
 
-const BOT_USERNAME = sdfsdfjsidjsjgjsdopgjd_bot
+const BOT_USERNAME = sdfsdfjsidjsjgjsdopgjd_bot;
 const app = express();
 const port = process.env.PORT || 3001;
 const token = process.env.TOKEN;
