08d3b6b6df27290c634b8dbb662f8431a4ad21be
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-06-11 18:31:40 +0200

    initial

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
+// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
-const User = require('.//models/user'); // Ensure the path is correct
+const User = require('./models/user');
 const axios = require('axios');
 
-// Additional logging to debug
-const fs = require('fs');
-console.log('Current directory:', __dirname);
-console.log('Files in directory:', fs.readdirSync(__dirname));
-console.log('Module paths:', module.paths);
-
-const BOT_USERNAME = "sdfsdfjsidjsjgjsdopgjd_bot"; // Replace with your bot's name
+const BOT_USERNAME = "sdfsdfjsidjsjgjsdopgjd_bot";
 const app = express();
 const port = process.env.PORT || 3001;
-const token = process.env.TOKEN;  // Убедитесь, что это указано в .env файле
-
+const token = process.env.TOKEN;
 let bot;
+
 try {
   bot = new TelegramBot(token, { polling: true });
 } catch (error) {
@@ -31,25 +26,34 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.log(error));
 
-const generateReferralCode = () => Math.random().toString(36).substr(2, 9);
+const generateReferralCode = () => {
+  return Math.random().toString(36).substr(2, 9);
+};
 
-const generateTelegramLink = (referralCode) => `https://t.me/${BOT_USERNAME}?start=${referralCode}`;
+const generateTelegramLink = (referralCode) => {
+  return `https://t.me/${BOT_USERNAME}?start=${referralCode}`;
+};
 
 const getProfilePhotoUrl = async (telegramId) => {
   try {
     const response = await axios.get(`https://api.telegram.org/bot${token}/getUserProfilePhotos`, {
-      params: { user_id: telegramId, limit: 1 },
+      params: {
+        user_id: telegramId,
+        limit: 1
+      }
     });
 
     if (response.data.ok && response.data.result.photos.length > 0) {
       const fileId = response.data.result.photos[0][0].file_id;
       const fileResponse = await axios.get(`https://api.telegram.org/bot${token}/getFile`, {
-        params: { file_id: fileId },
+        params: {
+          file_id: fileId
+        }
       });
 
       if (fileResponse.data.ok) {
@@ -78,7 +82,7 @@ app.post('/register', async (req, res) => {
         referralCode: user.referralCode,
         telegramLink: generateTelegramLink(user.referralCode),
         profilePhotoUrl: user.profilePhotoUrl,
-        gameProgress: user.gameProgress,
+        gameProgress: user.gameProgress
       });
     }
 
@@ -96,10 +100,10 @@ app.post('/register', async (req, res) => {
         upgrades: {
           coinPerClick: { level: 1, cost: 10 },
           energy: { level: 1, cost: 100, limit: 1000 },
-          energyTime: { level: 1, cost: 200, time: 2000, val: 0.5 },
+          energyTime: { level: 1, cost: 200, time: 2000, val: 0.5 }
         },
-        miniGameState: {},
-      },
+        miniGameState: {}
+      }
     });
 
     await user.save();
@@ -117,7 +121,7 @@ app.post('/register', async (req, res) => {
       referralCode: user.referralCode,
       telegramLink: generateTelegramLink(user.referralCode),
       profilePhotoUrl: user.profilePhotoUrl,
-      gameProgress: user.gameProgress,
+      gameProgress: user.gameProgress
     });
   } catch (error) {
     console.error('Error registering user:', error);
@@ -125,67 +129,142 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.get('/load-progress', async (req, res) => {
+app.get('/username', async (req, res) => {
   const userId = req.query.userId;
-
   try {
-    const user = await User.findById(userId);
+    const user = await User.findById(userId).populate('referredBy');
     if (user) {
-      res.json({ success: true, gameProgress: user.gameProgress });
+      const referralCount = await User.countDocuments({ referredBy: user._id });
+      res.json({
+        username: user.username,
+        coins: user.coins,
+        referralCode: user.referralCode,
+        telegramLink: generateTelegramLink(user.referralCode),
+        referralCount,
+        profilePhotoUrl: user.profilePhotoUrl,
+        gameProgress: user.gameProgress
+      });
     } else {
       res.status(404).json({ error: 'User not found' });
     }
   } catch (error) {
-    console.error('Error loading progress:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.post('/save-progress', async (req, res) => {
-  const { userId, coins, upgrades, miniGameState } = req.body;
-
+app.post('/check-subscription', async (req, res) => {
+  const { userId } = req.body;
   try {
     const user = await User.findById(userId);
-    if (user) {
-      user.coins = coins;
-      user.gameProgress = { upgrades, miniGameState };
-      await user.save();
-      res.json({ success: true, gameProgress: user.gameProgress });
-    } else {
-      res.status(404).json({ error: 'User not found' });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  } catch (error) {
-    console.error('Error saving progress:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 
-if (bot) {
-  bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const username = msg.from.username;
-
-    let profilePhotoUrl = await getProfilePhotoUrl(chatId);
-
-    let user = await User.findOneAndUpdate(
-      { telegramId: chatId.toString() },
-      {
-        telegramId: chatId.toString(),
-        username,
-        profilePhotoUrl,
-      },
-      { upsert: true, new: true }
-    );
-
-    await bot.sendMessage(chatId, 'Добро пожаловать! Нажмите на кнопку, чтобы начать игру.', {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Играть', web_app: { url: `${process.env.FRONTEND_URL}?userId=${user._id}` } }],
-        ],
-      },
+    const channelId = '@Clickerroadtomoon';
+    const response = await axios.get(`https://api.telegram.org/bot${token}/getChatMember`, {
+      params: {
+        chat_id: channelId,
+        user_id: user.telegramId
+      }
     });
+
+    if (response.data.ok) {
+      const isSubscribed = ['member', 'administrator', 'creator'].includes(response.data.result.status);
+      if (isSubscribed) {
+        user.coins += 50000;
+        await user.save();
+        res.json({ success: true, isSubscribed });
+      } else {
+        res.json({ success: false, isSubscribed });
+      }
+    } else {
+      res.status(400).json({ error: response.data.description });
+    }
+  } catch (error) {
+    console.error('Error checking subscription:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/update-coins', async (req, res) => {
+  const { userId, coins } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, { coins }, { new: true });
+    if (user) {
+      res.json({ success: true });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error updating coins:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Сохранение прогресса игры
+app.post('/save-progress', async (req, res) => {
+    const { userId, coins, upgrades, miniGameState } = req.body;
+  
+    try {
+      const user = await User.findById(userId);
+      if (user) {
+        user.coins = coins;
+        user.gameProgress = { upgrades, miniGameState };
+        await user.save();
+        res.json({ success: true, gameProgress: user.gameProgress });
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    } catch (error) {
+      console.error('Error saving progress:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   });
-}
+
+// Загрузка прогресса игры
+app.get('/load-progress', async (req, res) => {
+    const userId = req.query.userId;
+  
+    try {
+      const user = await User.findById(userId);
+      if (user) {
+        res.json({ success: true, gameProgress: user.gameProgress });
+      } else {
+        res.status(404).json({ error: 'Progress not found' });
+      }
+    } catch (error) {
+      console.error('Error loading progress:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+// Настройка Telegram Bot
+if (bot) {
+    bot.on('message', async (msg) => {
+      const chatId = msg.chat.id;
+      const username = msg.from.username;
+  
+      let profilePhotoUrl = await getProfilePhotoUrl(chatId);
+  
+      let user = await User.findOneAndUpdate(
+        { telegramId: chatId.toString() },
+        {
+          telegramId: chatId.toString(),
+          username: username,
+          profilePhotoUrl
+        },
+        { upsert: true, new: true }
+      );
+  
+      await bot.sendMessage(chatId, 'Добро пожаловать! Нажмите на кнопку, чтобы начать игру.', {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: 'Играть', web_app: { url: `${process.env.FRONTEND_URL}?userId=${user._id}` } }]
+          ]
+        }
+      });
+    });
+  }
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
