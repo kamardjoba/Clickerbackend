f08713464e8ad34f514ed8fc2fd91d55501f7e9f
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-06-19 01:10:42 +0200

    initial

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 require('dotenv').config();
-const UserProgress = require('./userProgres');
+const UserProgress = require('./models/userProgress');
 
 const app = express();
 const port = process.env.PORT || 3001;
