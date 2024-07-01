fe9e07d7c9fd3e7b4fce8eb86f0d362054bdec51
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-07-02 00:37:16 +0200

    initial

--- models/userProgress.js
+++ models/userProgress.js
@@ -84,6 +84,6 @@ const userProgressSchema = new mongoose.Schema({
      }]
 });
 
-const UserProgress = mongoose.model('User3Progresprogres', userProgressSchema);
+const UserProgress = mongoose.model('User4Progresprogres', userProgressSchema);
 
 module.exports = UserProgress;
