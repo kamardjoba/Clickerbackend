7645bde9582585b00aa6c1583c3e37e90fb680ec
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-07-02 00:22:48 +0200

    initial

--- index.js
+++ index.js
@@ -135,6 +135,7 @@ app.post('/check-subscription', async (req, res) => {
   }
 });
 
+
 app.post('/save-progress', async (req, res) => {
   const { userId, coins, upgradeCost, upgradeLevel, coinPerClick, upgradeCostEnergy, upgradeLevelEnergy, clickLimit, energyNow, upgradeCostEnergyTime, valEnergyTime, time } = req.body;
 
