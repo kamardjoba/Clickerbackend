94f39e8a2bbcfe46304d8a9950c24314f817e2d0
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-07-06 01:54:03 +0200

    initial

--- index.js
+++ index.js
@@ -97,27 +97,6 @@ async function updateProfilePhoto(telegramId) {
   }
 }
 
-// index.js (backend)
-app.post('/update-profile-photos', async (req, res) => {
-  const { telegramIds } = req.body;
-
-  try {
-    const updatedProfiles = await Promise.all(telegramIds.map(async (telegramId) => {
-      const profilePhotoUrl = await getProfilePhotoUrl(telegramId);
-      return {
-        telegramId,
-        profilePhotoUrl
-      };
-    }));
-
-    res.json({ success: true, updatedProfiles });
-  } catch (error) {
-    console.error('Error updating profile photos:', error);
-    res.status(500).json({ success: false, message: 'Error updating profile photos.' });
-  }
-});
-
-
 app.post('/check-subscription', async (req, res) => {
   const { userId } = req.body;
 
@@ -133,7 +112,6 @@ app.post('/check-subscription', async (req, res) => {
         user_id: user.telegramId
       }
     });
-
 
     const status = chatMemberResponse.data.result.status;
     const isSubscribed = ['member', 'administrator', 'creator'].includes(status);
@@ -198,8 +176,6 @@ app.post('/check-chat-subscription', async (req, res) => {
     res.status(500).json({ success: false, message: 'Error checking subscription.' });
   }
 });
-
-
 
 
 app.post('/save-progress', async (req, res) => {
@@ -455,5 +431,3 @@ bot.on('message', async (msg) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
