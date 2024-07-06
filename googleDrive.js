138e76db8f5c481d09a69db84748dfdcbf1fd99d
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-07-07 00:31:11 +0200

    initial

--- googleDrive.js
+++ googleDrive.js
@@ -5,8 +5,20 @@ const path = require('path');
 const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 const TOKEN_PATH = 'token.json';
 
-const credentials = require(path.join(__dirname, 'credentials.json'));
-const { client_secret, client_id, redirect_uris } = credentials.installed;
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+};
+
+const { client_secret, client_id, redirect_uris } = credentials;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
 oAuth2Client.setCredentials(require('./token.json'));
