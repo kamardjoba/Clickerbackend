4d34762da23a162b136ff1ea2abbdbe27b1aeba5
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-07-07 00:18:15 +0200

    initial

--- googleDrive.js
+++ googleDrive.js
@@ -1,4 +1,3 @@
-// googleDrive.js
 const { google } = require('googleapis');
 const fs = require('fs');
 const path = require('path');
@@ -6,62 +5,15 @@ const path = require('path');
 const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 const TOKEN_PATH = 'token.json';
 
-// Используем __dirname для построения абсолютного пути
-const credentialsPath = path.join(__dirname, 'credentials.json');
-console.log('Credentials Path:', credentialsPath); // Для отладки
-console.log('File exists:', fs.existsSync(credentialsPath)); // Для отладки
-
-if (!fs.existsSync(credentialsPath)) {
-  console.error('Файл credentials.json не найден');
-  process.exit(1);
-}
-
-const credentials = require(credentialsPath);
-
+const credentials = require(path.join(__dirname, 'credentials.json'));
 const { client_secret, client_id, redirect_uris } = credentials.installed;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
-function authorize() {
-  return new Promise((resolve, reject) => {
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getNewToken(oAuth2Client, resolve, reject);
-      oAuth2Client.setCredentials(JSON.parse(token));
-      resolve(oAuth2Client);
-    });
-  });
-}
+oAuth2Client.setCredentials(require('./token.json'));
 
-function getNewToken(oAuth2Client, resolve, reject) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) {
-        console.error('Error while trying to retrieve access token', err);
-        return reject(err);
-      }
-      oAuth2Client.setCredentials(token);
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      resolve(oAuth2Client);
-    });
-  });
-}
+const drive = google.drive({ version: 'v3', auth: oAuth2Client });
 
 async function uploadFile(filePath, fileName) {
-  const auth = await authorize();
-  const drive = google.drive({ version: 'v3', auth });
-
   const fileMetadata = {
     name: fileName,
     parents: [process.env.GOOGLE_DRIVE_FOLDER_ID], // ID папки в Google Drive
