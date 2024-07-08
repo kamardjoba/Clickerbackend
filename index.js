5e1bf0da9c445e53eb571ff213cb45c729f2d03f
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-07-08 03:03:20 +0200

    initial

--- index.js
+++ index.js
@@ -361,14 +361,14 @@ bot.on('message', async (msg) => {
       await updateProfilePhoto(userId);
     }
 
-    // const telegramLink = generateTelegramLink(user.referralCode);
-    // await bot.sendMessage(userId, `Добро пожаловать! Нажмите на кнопку, чтобы начать игру. Ваш реферальный код: ${user.referralCode}. Пригласите друзей по ссылке: ${telegramLink}`, {
-    //   reply_markup: {
-    //     inline_keyboard: [
-    //       [{ text: 'Играть', web_app: { url: `${process.env.FRONTEND_URL}?userId=${user._id}` } }]
-    //     ]
-    //   }
-    // });
+    const telegramLink = generateTelegramLink(user.referralCode);
+    await bot.sendMessage(userId, `Добро пожаловать! Нажмите на кнопку, чтобы начать игру. Ваш реферальный код: ${user.referralCode}. Пригласите друзей по ссылке: ${telegramLink}`, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Играть', web_app: { url: `${process.env.FRONTEND_URL}?userId=${user._id}` } }]
+        ]
+      }
+    });
   } catch (error) {
     console.error('Error in message handler:', error);
     bot.sendMessage(userId, `Произошла ошибка при обработке вашего сообщения.`);
