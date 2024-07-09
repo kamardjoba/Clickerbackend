1a9a01c415cb51a989ca00f8e76df8c433d5e4f9
Назар Лимар <nazarlimar@MacBook-Pro-Nazar.local>
2024-07-09 22:05:25 +0200

    initial

--- models/userProgress.js
+++ models/userProgress.js
@@ -85,6 +85,6 @@ const userProgressSchema = new mongoose.Schema({
      }]
 });
 
-const UserProgress = mongoose.model('User8Progresprogres', userProgressSchema);
+const UserProgress = mongoose.model('User7Progresprogres', userProgressSchema);
 
 module.exports = UserProgress;
